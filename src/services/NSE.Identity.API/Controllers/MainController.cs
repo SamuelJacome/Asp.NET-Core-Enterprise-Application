9fe52e8d7f472c39f46676f5bed23a7a2145c5e1
Samuel  Jácome <samuel.victorengenharia@gmail.com>
2022-05-23 10:18:26 -0300

    Add AuthController and MainController

---
+++ src/services/NSE.Identity.API/Controllers/MainController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace NSE.Identity.API.Controllers
+{
+    [ApiController]
+    public abstract class MainController : Controller
+    {
+        protected ICollection<string> Errors = new List<string>();
+
+        protected ActionResult CustomResponse(object result = null)
+        {
+            if (isValidOperation())
+            {
+                return Ok(result);
+            }
+            return BadRequest(new ValidationProblemDetails(new Dictionary<string, string[]>
+        {
+          {"Messagens", Errors.ToArray()}
+        }));
+        }
+
+        protected ActionResult CustomResponse(ModelStateDictionary modelState)
+        {
+            var errors = modelState.Values.SelectMany(e => e.Errors);
+            foreach (var error in errors)
+            {
+                AddError(error.ErrorMessage);
+            }
+            return CustomResponse();
+        }
+        protected bool isValidOperation()
+        {
+            return !Errors.Any();
+        }
+
+        protected void AddError(string error)
+        {
+            Errors.Add(error);
+        }
+
+        protected void ClearErrors(string error)
+        {
+            Errors.Clear();
+        }
+    }
+
+
+}
+
